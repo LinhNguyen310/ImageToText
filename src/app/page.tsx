169e0297b8d562e412516f670ac72ef2a8db54f7
Evelyn <lhnguye1@ualberta.ca>
2024-03-20 11:34:30 -0600

    Bug: Fix drag and drop images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,24 @@
 "use client";
 import { useState, useRef, useEffect } from 'react';
-import { Input } from "../../components/ui/input"
-import { Label } from "@/components/ui/label"
+import PreviewModal from '@/components/ui/PreviewModal';
 import { Button } from "@/components/ui/button"
 import { ScrollArea } from "@/components/ui/scroll-area"
-import { saveAs } from 'file-saver';
 import * as React from "react";
 import { Progress } from "@/components/ui/progress";
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card"
 export default function InputFile() {
-  const [progress, setProgress] = React.useState(0);
-  const [imageDetails, setImageDetails] = React.useState({ name: '', size: '', timeLeft: '' });
   const [images, setImages] = React.useState([]);
   const [previewText, setPreviewText] = useState<string>('');
   const [imageData, setImageData] = useState([]);
   const fileInputRef = useRef(null);
   const [hoverStates, setHoverStates] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isTranslationDone, setIsTranslationDone] = useState(false);
+  const [items, setItems] = useState([]);
+
   useEffect(() => {
     setHoverStates(new Array(imageData.length).fill(false));
   }, [imageData]);
@@ -48,14 +44,18 @@ export default function InputFile() {
     setImageData(items);
   };
 
-const handleDragEnd = () => {
-  setDraggingIndex(-1);
-};
-  const handleDownload = () => {
-    const blob = new Blob([previewText], {type: "text/plain;charset=utf-8"});
-    saveAs(blob, "output.doc");
+  const handleDragEnd = () => {
+    setItems(prevItems => {
+      if (draggingIndex < 0 || draggingIndex >= prevItems.length) {
+        return prevItems;
+      }
+      const newItems = [...prevItems];
+      const draggedItem = newItems.splice(draggingIndex, 1)[0];
+      newItems.push(draggedItem);
+      return newItems;
+    });
+    setDraggingIndex(-1);
   };
-
   const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
   };
@@ -68,8 +68,28 @@ const handleDragEnd = () => {
     event.preventDefault();
     if (event.dataTransfer.items && event.dataTransfer.items.length > 0) {
       handleImageUpload({ target: { files: event.dataTransfer.files } });
+      // Convert FileList to Array and map to their URLs
+      const droppedImages = Array.from(event.dataTransfer.files).map(file =>
+        URL.createObjectURL(file)
+      );
+      console.log(droppedImages)
+      // Create new image data objects
+      const newImageData = droppedImages.map(url => ({ url }));
+      // Update the images and imageData states
+      setImages(prevImages => [...prevImages, ...droppedImages]);
+      setImageData(prevImageData => [...prevImageData, ...newImageData]);
     }
   };
+  // Listen for changes in the images state
+  useEffect(() => {
+    console.log(images);
+  }, [images]);
+
+  function handleDelete(index: number) {
+    const deletedImage = imageData[index];
+    setImageData(prevImageData => prevImageData.filter((_, i) => i !== index));
+    setImages(prevImages => prevImages.filter(imageUrl => imageUrl !== deletedImage.url));
+  }
 
   function handleMouseEnter(index) {
     setHoverStates(prevHoverStates => prevHoverStates.map((hoverState, i) => i === index ? true : hoverState));
@@ -80,20 +100,6 @@ const handleDragEnd = () => {
     setHoverStates(prevHoverStates => prevHoverStates.map(() => false));
   }
 
-  // const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   if (event.target.files) {
-  //     Array.from(event.target.files).forEach(file => {
-  //       const reader = new FileReader();
-  //       reader.onloadend = () => {
-  //         const imageUrl = reader.result as string;
-  //         if (!images.includes(imageUrl)) {
-  //           setImages(prevImages => [...prevImages, imageUrl]);
-  //         }
-  //       };
-  //       reader.readAsDataURL(file);
-  //     });
-  //   }
-  // };
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       Array.from(event.target.files).forEach(file => {
@@ -145,14 +151,10 @@ const handleDragEnd = () => {
     event.target.value = null;
   };
 
-  function handleDelete(index: number) {
-    const deletedImage = imageData[index];
-    setImageData(prevImageData => prevImageData.filter((_, i) => i !== index));
-    setImages(prevImages => prevImages.filter(imageUrl => imageUrl !== deletedImage.url));
-  }
 
   const handleTranslate = async () => {
-    console.log(images);
+    setIsLoading(true);
+
     let combinedText = '';
   
     const fetchPromises = images.map(async (imageUrl, index) => {
@@ -206,125 +208,131 @@ const handleDragEnd = () => {
         console.error('Upload failed.');
       }
     });
-  
     await Promise.all(fetchPromises);
+    setIsLoading(false);
+    setIsTranslationDone(true);
   };
   return (
-    // <div className="grid w-full max-w-sm items-center gap-1.5">
-    //   <Label htmlFor="picture">Picture</Label>
-    //   <Input id="picture" type="file" accept="image/*" name="file" onChange={handleImageChange} multiple />
-    //   <div className="image-container" style={{ display: 'flex', flexWrap: 'wrap' }}>
-    //     {images.map((image, index) => (
-    //       <img key={index} src={image} alt={` ${index}`} style={{ width: '100px', height: '50px' }} />
-    //     ))}
-    //   </div>
-    //   <Button onClick={handleTranslate}>Translate Images</Button>
-    //   <Label>Preview Text:</Label>
-    //   <textarea value={previewText} readOnly style={{ width: '500px', height: '200px' }} /> 
-    //   <Button onClick={handleDownload}>Download as Doc</Button>
-    //   </div>
-    <div style={{ 
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      width: '100%', 
-      height: '100vh', 
-      padding: "10px", 
-      fontFamily:"Verdana, sans-serif" 
-    }}>
+  <div style={{ 
+    display: 'flex', 
+    alignItems: 'center', 
+    width: '100%', 
+    height: '100vh', 
+    padding: "10px", 
+    fontFamily:"Verdana, sans-serif" 
+  }}>
+    <div style={{ width: '100%', display: 'flex', flexDirection: 'column', padding: "10px", gap:"10px" }}>
       <Card style={{ width: '100%', height: '50%'}}>
-      <CardContent style={{ display: 'flex', flexDirection: 'row', gap: '10px', height:"100%" }}>
-        <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <p style={{padding: "10px" }}></p>
-         <label htmlFor="picture" style={{ width: '100%', height: '100%', cursor: 'pointer' }}>
-          <Card             
-            onDragOver={handleDragOver}
-            onDragEnter={handleDragEnter}
-            onDrop={handleDrop}
-             style={{ border: '1px dashed #000', width: '100%', height: '100%', backgroundColor: 'rgb(250, 250, 250)' }}>
-              <input
-                ref={fileInputRef}
-                id="picture"
-                type="file"
-                accept="image/*"
-                name="file"
-                onChange={handleImageUpload}
-                multiple
-                style={{ display: 'none' }}
-              />
-            <CardContent style={{ display: 'flex', flexDirection: 'row' }}>
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '40%', padding:"10px" }}>
-              <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" style={{ width: '20%', height: 'auto' }} fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v9m-5 0H5a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1h-2M8 9l4-5 4 5m1 8h.01"/>
-              </svg>
-            </div>
-            <p style={{ textAlign: 'center' }}>            Drag and drop files here or click to select files</p>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '60%' }}>
-            </div>
-          </div>
-              </CardContent>
-          </Card>
-          </label>
-        </div>
-        <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4" style={{ border: 'transparent', width: '100%', height: '100%' }}>
-          <div className="image-container" style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap' , width:"100%"}}>
-            {imageData.map((data, index) => (
-            <Card
-            draggable="true"
-            onDragStart={(event) => handleDragStart(event, index)}
-            onDragOver={(event) => handleDragOverImage(event, index)}
-            onDragEnd={handleDragEnd}
-            key={index}
-            style={{
-              flex: 1,
-              border: '1.5px solid #D3D3D3',
-              cursor: 'pointer',
-              opacity: draggingIndex === index ? 0.5 : 1,
-            }}
-          >
-                <div style={{ display: 'flex', flexDirection: 'row' }}>
-                  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding:"10px" }}>
-                    <img src={data.url} alt={` ${index}`} style={{ width: '30px', height: '30px' }} />
+        <CardContent style={{ display: 'flex', flexDirection: 'row', gap: '10px', height:"100%" }}>
+          <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <p style={{padding: "10px" }}></p>
+            <label htmlFor="picture" style={{ width: '100%', height: '100%', cursor: 'pointer' }}>
+            <Card             
+              onDragOver={handleDragOver}
+              onDragEnter={handleDragEnter}
+              onDrop={handleDrop}
+              style={{ 
+                display: 'flex', 
+                justifyContent: 'center', 
+                alignItems: 'center', 
+                border: '1px dashed #000', 
+                width: '100%', 
+                height: '100%', 
+                backgroundColor: 'rgb(250, 250, 250)' 
+              }}
+            >
+              <CardContent style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%', paddingTop:"25%"}}>
+                <input
+                  ref={fileInputRef}
+                  id="picture"
+                  type="file"
+                  accept="image/*"
+                  name="file"
+                  onChange={handleImageUpload}
+                  multiple
+                  style={{ display: 'none' }}
+                />
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+                  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '40%', padding:"10px" }}>
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" style={{ width: '20%', height: 'auto' }} fill="none" viewBox="0 0 24 24">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v9m-5 0H5a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1h-2M8 9l4-5 4 5m1 8h.01"/>
+                    </svg>
                   </div>
-                  <div style={{ marginLeft: '10px', width:"100%", padding:"10px", fontSize: '0.8em' }}>
-                        <div style={{ display: 'flex' }}>
-                          <div style={{ flex: '0 0 100%' }}>
-                          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                          <div style={{ fontWeight: 'bold', fontSize: '0.8em' }}>{data.details.name}</div>
-                          <svg 
-                            onClick={() => handleDelete(index)}
-                            onMouseEnter={() => handleMouseEnter(index)}
-                            onMouseLeave={handleMouseLeave}
-                            className="w-5 h-5 text-gray-800 dark:text-white" 
-                            aria-hidden="true" 
-                            xmlns="http://www.w3.org/2000/svg" 
-                            width="16" 
-                            height="16" 
-                            fill={hoverStates[index] ? "red" : "gray"} 
-                            viewBox="0 0 24 24" 
-                          >
-                            <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
-                          </svg>
-                        </div>                          
-                              <div style={{ display: 'flex', justifyContent: 'space-between', color: 'gray',fontSize: '0.8em', padding:"3px" }}>
-                              <div>{data.details.size}</div>
-                              <div>{data.progress}%</div>
-                            </div>
-                          </div>
-                        </div>
-                      <Progress value={data.progress} color="green" style={{ alignSelf: 'stretch', height: '5px' , width:"100%"}} />
+                  <p style={{ textAlign: 'center', paddingTop:"10%" }}>Drag and drop files here or click to select files</p>
+                  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '60%' }}>
                   </div>
                 </div>
-              </Card>
-            ))}
+              </CardContent>
+            </Card>
+            </label>
           </div>
-        </ScrollArea>
-       </div>
-      </CardContent>
-    </Card>
-
+          <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4" style={{ border: 'transparent', width: '100%', height: '100%' }}>
+            <div className="image-container" style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap' , width:"100%"}}>
+              {imageData.map((data, index) => (
+              <Card
+              draggable="true"
+              onDragStart={(event) => handleDragStart(event, index)}
+              onDragOver={(event) => handleDragOverImage(event, index)}
+              onDragEnd={handleDragEnd}
+              key={index}
+              style={{
+                flex: 1,
+                border: '1.5px solid #D3D3D3',
+                cursor: 'pointer',
+                opacity: draggingIndex === index ? 0.5 : 1,
+              }}
+            >
+                  <div style={{ display: 'flex', flexDirection: 'row' }}>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding:"10px" }}>
+                      <img src={data.url} alt={` ${index}`} style={{ width: '30px', height: '30px' }} />
+                    </div>
+                    <div style={{ marginLeft: '10px', width:"100%", padding:"10px", fontSize: '0.8em' }}>
+                          <div style={{ display: 'flex' }}>
+                            <div style={{ flex: '0 0 100%' }}>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <div style={{ fontWeight: 'bold', fontSize: '0.8em' }}>{data.details.name}</div>
+                            <svg 
+                              onClick={() => handleDelete(index)}
+                              onMouseEnter={() => handleMouseEnter(index)}
+                              onMouseLeave={handleMouseLeave}
+                              className="w-5 h-5 text-gray-800 dark:text-white" 
+                              aria-hidden="true" 
+                              xmlns="http://www.w3.org/2000/svg" 
+                              width="16" 
+                              height="16" 
+                              fill={hoverStates[index] ? "red" : "gray"} 
+                              viewBox="0 0 24 24" 
+                            >
+                              <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
+                            </svg>
+                          </div>                          
+                                <div style={{ display: 'flex', justifyContent: 'space-between', color: 'gray',fontSize: '0.8em', padding:"3px" }}>
+                                <div>{data.details.size}</div>
+                                <div>{data.progress}%</div>
+                              </div>
+                            </div>
+                          </div>
+                        <Progress value={data.progress} color="green" style={{ alignSelf: 'stretch', height: '5px' , width:"100%"}} />
+                    </div>
+                  </div>
+                </Card>
+              ))}
+            </div>
+          </ScrollArea>
+        </div>
+        </CardContent>
+      </Card>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Button onClick={handleTranslate}>Translate Images</Button>
+        <PreviewModal 
+          isOpen={isLoading || isTranslationDone} 
+          onClose={() => { setPreviewText(""); setIsTranslationDone(false); }} 
+          previewText={previewText} 
+          isLoading={isLoading}
+        />
+      </div>
+    </div>
   </div>
   )
 }
