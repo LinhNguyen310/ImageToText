5cc55be620769efae5e0364b5838d0207edc092b
Evelyn <lhnguye1@ualberta.ca>
2024-03-20 16:46:43 -0600

    Bug: Fixed Upload type

--- pages/api/upload.tsx
+++ pages/api/upload.tsx
@@ -1,41 +1,36 @@
-"use server";
-import Cors from 'cors';
 import multer from 'multer';
 import { Storage } from '@google-cloud/storage';
-import { NextApiRequest, NextApiResponse } from 'next';
-import initMiddleware from '@/lib/init-middleware';
+
 const upload = multer({ storage: multer.memoryStorage() });
-interface MulterRequest extends NextApiRequest {
-    file: any; // or specify a more specific type
-}
-const cors = initMiddleware(
-    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-    Cors({
-      // Only allow requests with GET, POST and OPTIONS
-      methods: ['GET', 'POST', 'OPTIONS'],
-    })
-  )
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    await cors(req, res);
-    console.log(req.body)
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
     const gcs = new Storage({
-        projectId: process.env.GCP_PROJECT_ID,
-        credentials: {
-            client_email: process.env.GCP_CLIENT_EMAIL,
-            private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
-        },
+      projectId: process.env.GCP_PROJECT_ID,
+      credentials: {
+        client_email: process.env.GCP_CLIENT_EMAIL,
+        private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      },
     });
-    
-    const bucket = gcs.bucket(process.env.BUCKET_NAME || '');
-    const multerRequest = req as MulterRequest;
 
-    upload.single('file')(multerRequest as any, res as any, (err) => {
+    const bucket = gcs.bucket(process.env.BUCKET_NAME);
 
-        const fileName = multerRequest.file.originalname;
+    upload.single('file')(req, res, (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: err.message });
+        }
+
+        const fileName = req.file.originalname;
         const file = bucket.file(fileName);
         const blobStream = file.createWriteStream({
             metadata: {
-                contentType: multerRequest.file.mimetype,
+                contentType: req.file.mimetype,
             },
         });
 
@@ -58,12 +53,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         });
 
         // Pipe the file to the blobStream
-        blobStream.end(multerRequest.file.buffer);
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: err.message });
-        }
-  
+        blobStream.end(req.file.buffer);
     });
 }
-
